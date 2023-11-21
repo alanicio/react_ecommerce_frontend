f0220f3d153a80ef7705bd9b1e8ed54514f8ca2c
alanicio <alanicio98@outlook.com>
2023-11-21 05:39:31 -0600

    button now has CSS

--- src/components/FindItem/FindItem.jsx
+++ src/components/FindItem/FindItem.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const FindItem = () => {
   return (
-    <div className="row with-border-bottom pb-15">
+    <div className="row pb-15">
       <div className="col-100">
         <label htmlFor="item">Find Item</label>
         <input type="text" name="item" id="item" />
-        <button>Add Item</button>
+        <button className="primary">Add Item</button>
       </div>
     </div>
   );
