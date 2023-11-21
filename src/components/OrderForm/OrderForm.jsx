89f6c81af26528137fb115db6f5f98b956e678cf
alanicio <alanicio98@outlook.com>
2023-11-21 05:23:25 -0600

    added layout of first part of OrderForm

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./OrderForm.css";
 
 const OrderForm = () => {
-  return <div className="container">OrderForm</div>;
+  return (
+    <div className="container">
+      <header>
+        <h2>Orders Form</h2>
+      </header>
+      <div className="row with-border-bottom pb-15">
+        <div className="col-100">
+          <label htmlFor="item">Find Item</label>
+          <input type="text" name="item" id="item" />
+          <button>Add Item</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderForm;
